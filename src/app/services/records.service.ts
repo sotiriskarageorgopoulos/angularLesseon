106a0f423f08fafbd0e64a70b6ee228633c3916d
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-02-04 16:58:24 +0200

    Proxy Configuration

--- src/app/services/records.service.ts
+++ src/app/services/records.service.ts
@@ -4,11 +4,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class RecordsService {
 
   constructor(private http:HttpClient) { }
 
   getData(){ //return the data which contains db.json 
-    return  this.http.get('http://localhost:3000/person')
+    return  this.http.get('/api/db.json')
   }
 }
